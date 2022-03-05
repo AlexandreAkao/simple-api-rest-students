3a5bbc020dd29cd61aa136409cbe1ecdbe601091
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-03-05 04:12:39 -0300

    chore: add login jwt

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+
+class TokenController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        errors: ['Email and password are required'],
+      });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    const isPasswordValid = await user.passwordIsValid(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        errors: ['Email or password invalid'],
+      });
+    }
+
+    const token = jwt.sign({ id: user.id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    });
+
+    return res.json({ user, token });
+  }
+}
+
+export default new TokenController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import homeRouter from './homeRoutes';
 import userRouter from './userRoutes';
+import tokenRouter from './tokenRoutes';
 
 const router = Router();
 
 router.use('/', homeRouter);
 router.use('/users', userRouter);
+router.use('/tokens', tokenRouter);
 
 export default router;
