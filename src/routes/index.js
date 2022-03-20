15b492a7e50f2f0413b7c086dffd42c31b5598bc
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-03-19 21:34:41 -0300

    refactor: remove useless files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-import homeRouter from './homeRoutes';
 import userRouter from './userRoutes';
 import tokenRouter from './tokenRoutes';
 import studentRouter from './studentRoutes';
@@ -8,7 +7,6 @@ import imageRouter from './imageRoutes';
 
 const router = Router();
 
-router.use('/', homeRouter);
 router.use('/users', userRouter);
 router.use('/tokens', tokenRouter);
 router.use('/students', studentRouter);
