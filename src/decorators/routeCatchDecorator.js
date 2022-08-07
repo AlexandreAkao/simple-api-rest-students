73257e2afe465b59094f41637ac6dadd4f0770e4
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-08-07 20:18:02 -0300

    doc: add examples config

--- src/decorators/routeCatchDecorator.js
+++ src/decorators/routeCatchDecorator.js
@@ -2,6 +2,7 @@ export default (action) => async (req, res) => {
   try {
     return await action(req, res);
   } catch (error) {
+    console.log(error);
     return res.status(400).json({
       errors: error?.errors?.map((validationError) => validationError.message),
     });
